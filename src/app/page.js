8ae2d79dcd907e4dea1c597cf115fbd608243b19
naufalnabbih <naufalnabbih@gmail.com>
2023-10-16 11:25:29 +0700

    Tugas 2-Responsive Drawer

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,36 @@
-import Image from 'next/image'
-
 import Button from '@mui/material/Button';
-import { TextField } from '@mui/material';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Input from '@mui/material/Input';
+import FormHelperText from '@mui/material/FormHelperText';
 export default function Login() {
   return (
     <>
-      <h1 className='mt-20'>HomePage</h1>
-      <form>
-        {/* <labe>Username</labe>
+      <h1 className='mt-20'>Login</h1>
+      <FormControl>
+        <InputLabel htmlFor="my-input">Email address</InputLabel>
+        <Input id="my-input" aria-describedby="my-helper-text" />
+        <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+        <Button variant='contained'>Submit  </Button>
+      </FormControl>
+
+
+      {/* TextField and Button */}
+      {/* <TextField id="outlined-basic" label="Username" variant="outlined" />
+        <TextField id="filled-basic" label="Password" variant="filled" />
+        <Button variant='contained'>Login</Button> */}
+
+
+      {/* Input username and password with button Login */}
+      {/* <label>Username</label>
         <input type='text'></input>
         <label>Password</label>
         <input type='password'>
         </input>
-        <input type='submit' value={"Login"}></input> */}
-        <TextField id="outlined-basic" label="Username" variant="outlined" />
-        <TextField id="filled-basic" label="Password" variant="outlined" />
-        <Button variant="contained">Submit</Button>
-      </form>
+        <Button variant="contained">Login</Button> */}
+
     </>
   )
 }
